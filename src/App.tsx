42ea71d79b11bde5842de7edb281a9d09cc1b477
Tenzin Thobten Leduk <50613123+tenaureo@users.noreply.github.com>
2021-09-14 16:00:33 +0200

    Restructuring

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App(){
+  
+  const [nr, setNr] = useState(0);
 
-const [nr, setNr] = useState(0);
+  const plusOne = () => {
+    setNr(nr + 1);
+  }
 
-const plusOne = () => {
-  setNr(nr + 1);
-}
-
-const minusOne = () => {
-  setNr(nr - 1);
-}
+  const minusOne = () => {
+    setNr(nr - 1);
+  }
 
   return (
     <div className="App">
@@ -20,7 +20,5 @@ const minusOne = () => {
       <button onClick={minusOne}>-1</button>
     </div>  
   );
-  
 }
-
 export default App;
